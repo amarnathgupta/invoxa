fc26ee6d02d5c06a1235bf1fc32564bc80cfb9c0
Amar Nath Gupta <ang.amarnathgupta@gmail.com>
2026-02-26 23:22:10 +0530

    feat(organization): add get all org endpoint, refactor create org controller

--- src/controllers/organization.controller.ts
+++ src/controllers/organization.controller.ts
@@ -32,6 +32,17 @@ export const createOrganizationController = async (
     slug,
   } = result.data;
   try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      select: {
+        isVerified: true,
+      },
+    });
+    if (user && !user.isVerified) {
+      return errorResponse(res, 400, "User is not verified");
+    }
     const slugValue: string =
       slug ?? (await generateUniqueSlug(req.user.id, name));
     const organization = await prisma.organization.create({
@@ -64,6 +75,43 @@ export const createOrganizationController = async (
     if (e.code === "P2002") {
       return errorResponse(res, 400, "Slug already exists");
     }
+    return errorResponse(res, 500, "Internal server error");
+  }
+};
+
+export const getAllOrganizationController = async (
+  req: AuthRequest,
+  res: Response,
+) => {
+  if (!req?.user) {
+    return errorResponse(res, 401, "Unauthorized");
+  }
+  const { limit, page } = req.query;
+  const limitValue = limit ? parseInt(limit as string) : 10;
+  const pageValue = page ? parseInt(page as string) : 1;
+  try {
+    const organizations = await prisma.organization.findMany({
+      where: {
+        ownerId: req.user.id,
+        deletedAt: null,
+      },
+      take: limitValue,
+      skip: (pageValue - 1) * limitValue,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    if (organizations.length === 0) {
+      return errorResponse(res, 404, "No organizations found");
+    }
+    return successResponse(
+      res,
+      200,
+      "Organizations fetched successfully",
+      organizations,
+      { limit: limitValue, page: pageValue, total: organizations.length },
+    );
+  } catch (error) {
     return errorResponse(res, 500, "Internal server error");
   }
 };
--- src/routes/organization.route.ts
+++ src/routes/organization.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createOrganizationController } from "../controllers/organization.controller";
+import {
+  createOrganizationController,
+  getAllOrganizationController,
+} from "../controllers";
 import { authMiddleware } from "../middlewares";
 
 export const organizationRouter = Router();
 
 organizationRouter.use(authMiddleware);
 organizationRouter.post("/", createOrganizationController);
+organizationRouter.get("/", getAllOrganizationController);
